0247471ec2a82d06bd7b67b03f63fbc34fd00a02
Matt Reed <matthewreed26@gmail.com>
2023-03-24 03:21:12 -0400

    Apply java-base module

---
+++ src/main/java/com/healthAndHome/error/package-info.java
@@ -0,0 +1,2 @@
+@com.healthAndHome.SharedKernel
+package com.healthAndHome.error;
---
+++ src/test/java/com/healthAndHome/common/domain/HealthAndHomeCollectionsTest.java
@@ -0,0 +1,89 @@
+package com.healthAndHome.common.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import com.healthAndHome.UnitTest;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+@UnitTest
+class HealthAndHomeCollectionsTest {
+
+  @Nested
+  @DisplayName("Collections")
+  class HealthAndHomeCollectionsCollectionsTest {
+
+    @Test
+    void shouldGetEmptyImmutableCollectionFromNullCollection() {
+      Collection<Object> input = null;
+      Collection<Object> collection = HealthAndHomeCollections.immutable(input);
+
+      assertThat(collection).isEmpty();
+      assertThatThrownBy(() -> collection.clear()).isExactlyInstanceOf(UnsupportedOperationException.class);
+    }
+
+    @Test
+    void shouldGetImmutableCollectionFromMutableCollection() {
+      Collection<String> input = new ArrayList<>();
+      input.add("value");
+      Collection<String> collection = HealthAndHomeCollections.immutable(input);
+
+      assertThat(collection).containsExactly("value");
+      assertThatThrownBy(() -> collection.clear()).isExactlyInstanceOf(UnsupportedOperationException.class);
+    }
+  }
+
+  @Nested
+  @DisplayName("Set")
+  class HealthAndHomeCollectionsSetTest {
+
+    @Test
+    void shouldGetEmptyImmutableCollectionFromNullCollection() {
+      Set<Object> input = null;
+      Set<Object> set = HealthAndHomeCollections.immutable(input);
+
+      assertThat(set).isEmpty();
+      assertThatThrownBy(() -> set.clear()).isExactlyInstanceOf(UnsupportedOperationException.class);
+    }
+
+    @Test
+    void shouldGetImmutableCollectionFromMutableCollection() {
+      Set<String> input = new HashSet<>();
+      input.add("value");
+      Set<String> set = HealthAndHomeCollections.immutable(input);
+
+      assertThat(set).containsExactly("value");
+      assertThatThrownBy(() -> set.clear()).isExactlyInstanceOf(UnsupportedOperationException.class);
+    }
+  }
+
+  @Nested
+  @DisplayName("List")
+  class HealthAndHomeCollectionsListTest {
+
+    @Test
+    void shouldGetEmptyImmutableCollectionFromNullCollection() {
+      List<Object> input = null;
+      List<Object> list = HealthAndHomeCollections.immutable(input);
+
+      assertThat(list).isEmpty();
+      assertThatThrownBy(() -> list.clear()).isExactlyInstanceOf(UnsupportedOperationException.class);
+    }
+
+    @Test
+    void shouldGetImmutableCollectionFromMutableCollection() {
+      List<String> input = new ArrayList<>();
+      input.add("value");
+      List<String> list = HealthAndHomeCollections.immutable(input);
+
+      assertThat(list).containsExactly("value");
+      assertThatThrownBy(() -> list.clear()).isExactlyInstanceOf(UnsupportedOperationException.class);
+    }
+  }
+}
