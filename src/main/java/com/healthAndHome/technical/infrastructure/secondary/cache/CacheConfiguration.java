04d28654116ee1d15ae195f450e41505164ef907
Matt Reed <matthewreed26@gmail.com>
2023-05-23 09:44:03 -0400

    Apply spring-boot-cache module

---
+++ src/main/java/com/healthAndHome/technical/infrastructure/secondary/cache/CacheConfiguration.java
@@ -0,0 +1,8 @@
+package com.healthAndHome.technical.infrastructure.secondary.cache;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+class CacheConfiguration {}
